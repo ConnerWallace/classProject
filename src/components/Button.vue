de0237040e10fe36a83e562853385fa68f511472
Conner Wallace <wall8402@kettering.edu>
2020-02-16 14:41:00 -0500

    button.vue export fixed

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,3 +1,15 @@
+<template>
 <div>
    <b-col lg="4" class="pb-2"><b-button size="lg">Current Courses</b-button></b-col>
 </div>
+</template>
+
+<script>
+export default {
+  name: 'Button', //this is the name of the component,
+}
+</script>
+
+
+<style>
+</style>
