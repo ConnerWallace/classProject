848600851d8c45cf68c488a40f2d760e273f92eb
Connnor <powersgg@gmail.com>
2020-02-16 11:53:39 -0500

    change in home.vue add button.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,6 +11,18 @@ export default {
     'Calendar': Calendar
   }
 }
+
 </script>
+
+<div align= "left"> //puts button on left
+import Button from './Button.vue'
+export default {
+  name: 'Home', //this is the name of the component,
+  components: {
+    'Button': Button
+  }
+}
+</div>
+
 <style>
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './Router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
+import { ButtonPlugin } from 'bootstrap-vue'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+//Install B-Button
+Vue.use(ButtonPlugin)
 
 // app.js
 import 'bootstrap/dist/css/bootstrap.css'
