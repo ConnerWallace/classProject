1695e737301428e25c0169489cf2b05d23645402
Connnor <powersgg@gmail.com>
2020-02-16 13:46:05 -0500

    fixed home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,19 +7,15 @@
 
 <script>
 import Calendar from './Calendar.vue'
-export default {
-  name: 'Home', //this is the name of the component,
-  components: {
-    'Calendar': Calendar,
-  }
-}
 import Button from './Button.vue'
 export default {
   name: 'Home', //this is the name of the component,
   components: {
+    'Calendar': Calendar,
     'Button': Button,
   }
 }
+
 </script>
 
 
