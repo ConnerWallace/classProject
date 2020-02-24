b1f41b3ad9b5f0ea2eb3ace1e6cc2cbb13e608aa
N0manKet <n0manscholar@gmail.com>
2020-02-24 08:03:41 -0500

    Prototype

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,9 +1,68 @@
 <template>
-  <button type="button">Template</button>
+  <div id="homeContainer">
+    <b-list-group horizontal>
+      <b-card
+        title="CS 101"
+        img-src="./classroom.jpeg"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text></b-card-text>
+
+        <b-button href="#" variant="primary">Enter Class</b-button>
+      </b-card>,
+      <b-card
+        title="MATH 101"
+        img-src="./classroom.jpeg"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text></b-card-text>
+
+        <b-button href="#" variant="primary">Enter Class</b-button>
+      </b-card>,
+      <b-card
+        title="COMM 101"
+        img-src="./classroom.jpeg"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text></b-card-text>
+
+        <b-button href="#" variant="primary">Enter Class</b-button>
+      </b-card>,
+      <b-card
+        title="BIO 101"
+        img-src="./classroom.jpeg"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text></b-card-text>
+
+        <b-button href="#" variant="primary">Enter Class</b-button>
+      </b-card>
+,
+    </b-list-group>
+  </div>
 </template>
 
 <script>
 // In your Vue.js component.
+
+//Classroom.jpeg <https://www.pexels.com/photo/abc-books-chalk-chalkboard-265076/>
+
 
 export default {
   name: "Courses", //this is the name of the component,
