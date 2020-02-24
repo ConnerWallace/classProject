a53c426817fde847533cd5d550cab0bde357999f
N0manKet <n0manscholar@gmail.com>
2020-02-24 07:07:40 -0500

    Template

--- src/Router/index.js
+++ src/Router/index.js
@@ -6,6 +6,7 @@ import Home from '@/components/Home'
 import About from '@/components/About'
 import Contact from '@/components/Contact'
 import Login from '@/components/Login'
+import Courses from '@/components/Courses'
 
 Vue.use(Router)
 
@@ -36,5 +37,10 @@ export default new Router({
       name: 'Login',
       component: Login
     },
+    {
+      path: '/courses',
+      name: 'Courses',
+      component: Courses
+    }
   ]
 })
---
+++ src/components/Courses.vue
@@ -0,0 +1,15 @@
+<template>
+  <button type="button">Template</button>
+</template>
+
+<script>
+// In your Vue.js component.
+
+export default {
+  name: "Courses", //this is the name of the component,
+  components: {},
+  data: () => ({})
+};
+</script>
+<style>
+</style>
