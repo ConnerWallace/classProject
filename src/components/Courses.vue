2c2f52d8896921259abe980cfd4f8a7e7de5b564
N0manKet <n0manscholar@gmail.com>
2020-02-24 16:40:52 -0500

    Static to V_for

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,59 +1,20 @@
 <template>
-  <div id="homeContainer">
+  <div id="coursesContainer">
     <b-list-group horizontal>
+      <b-list-group-item v-for="course in courses" v-bind:key="course.id">
       <b-card
-        title="CS 101"
-        img-src="./classroom.jpeg"
-        img-alt="Image"
+        :title="course.title"
+        :img-src="course.imgSrc"
+        :img-alt="course.imgAlt"
         img-top
-        tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
+        :tag="course.tag"
+        :class="course.class"
       >
         <b-card-text></b-card-text>
 
         <b-button href="#" variant="primary">Enter Class</b-button>
       </b-card>,
-      <b-card
-        title="MATH 101"
-        img-src="./classroom.jpeg"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
-        <b-card-text></b-card-text>
-
-        <b-button href="#" variant="primary">Enter Class</b-button>
-      </b-card>,
-      <b-card
-        title="COMM 101"
-        img-src="./classroom.jpeg"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
-        <b-card-text></b-card-text>
-
-        <b-button href="#" variant="primary">Enter Class</b-button>
-      </b-card>,
-      <b-card
-        title="BIO 101"
-        img-src="./classroom.jpeg"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
-        <b-card-text></b-card-text>
-
-        <b-button href="#" variant="primary">Enter Class</b-button>
-      </b-card>
-,
+      </b-list-group-item>
     </b-list-group>
   </div>
 </template>
@@ -63,12 +24,52 @@
 
 //Classroom.jpeg <https://www.pexels.com/photo/abc-books-chalk-chalkboard-265076/>
 
-
 export default {
   name: "Courses", //this is the name of the component,
   components: {},
-  data: () => ({})
+  data: () => ({
+    courses: [
+      {
+        id: 0,
+        title: "./classroom.jpeg",
+        imgSrc: "./classroom.jpeg",
+        imgAlt: "Image",
+        tag: "article",
+        class: "coursesContainer"
+      },
+      {
+        id: 1,
+        imageLocation: "./classroom.jpeg",
+        title: "./classroom.jpeg",
+        imgSrc: "./classroom.jpeg",
+        imgAlt: "Image",
+        tag: "article",
+        class: "coursesContainer"
+      },
+      {
+        id: 2,
+        imageLocation: "./classroom.jpeg",
+        title: "./classroom.jpeg",
+        imgSrc: "./classroom.jpeg",
+        imgAlt: "Image",
+        tag: "article",
+        class: "coursesContainer"
+      },
+      {
+        id: 3,
+        title: "./classroom.jpeg",
+        imgSrc: "./classroom.jpeg",
+        imgAlt: "Image",
+        tag: "article",
+        class: "coursesContainer"
+      }
+    ]
+
+  })
 };
 </script>
 <style>
+.coursesContainer {
+  max-width: 20rem;
+}
 </style>
