4605bd038a6fa57aad37f4e8378fffa2bcf99f73
case2223 <case2223@kettering.edu>
2020-02-24 13:55:33 -0500

    added records file

---
+++ src/components/Records.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div>
+    <h1>Student Records</h1>
+    <b-col lg="4" class="pb-2"><b-button size="lg" href="/Past Records" >Records</b-button></b-col>
+  <p>
+    <ul>
+      <li>Curent Grades: {{grades}}</li>
+      <li>Overall GPA: {{gpa}}</li>
+      <li>Past Records: {{pastRecords}}</li>
+    </ul>
+  </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data(){
+    return {
+      grades: 'Class 1 - A Class 2 - B',
+      gpa: 'Overall GPA: 3.47',
+      
+    }
+  }
+}
+</script>
